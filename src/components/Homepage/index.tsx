4d16fce86e46ac25d6f4bb6ca64a09d19523d10c
Ilya Spiridonov <spiridonov.ilya@gmail.com>
2022-09-02 15:20:52 +0300

    Content update (texts, features, cases)

--- src/components/Homepage/index.tsx
+++ src/components/Homepage/index.tsx
@@ -60,11 +60,10 @@ export default function Homepage(): JSX.Element {
                 <div className="homepage__container--adaptive">
                     <div className="homepage__flex flex-l-6 flex-m-5">
                         <div className="main__title">Continuous Localization for GitHub Projects</div>
-                        <div className="main__description">A better process to translate products, {/*<br/>*/}
-                            documentation or websites</div>
+                        <div className="main__description">Create sustainable translation workflows with the docs-as-code approach at the core</div>
                         <div className="main__actions">
                             {!authorization.token && <Button className="github-button" href="https://gitlocalize.com/auth/grant">Get started with GitHub</Button>}
-                            <Button className="demo-button" href="#demo">Watch a Demo</Button>
+                            <Button className="demo-button" href="#demo">Watch Demo</Button>
                         </div>
                     </div>
                     <div className="homepage__flex main__app-img-container flex-l-4 flex-m-5">
@@ -77,8 +76,8 @@ export default function Homepage(): JSX.Element {
                 <div className="homepage__container--adaptive">
                     <div className="homepage__flex">
                         <div className="features__title">Features</div>
-                        <div className="features__description">A better process to translate products, documentation or websites better process
-                            to translate products, documentation or websites</div>
+                        <div className="features__description">Here's what makes GitLocalize a great fit for GitHub-based localization projects:
+                        </div>
                     </div>
                 </div>
             </div>
@@ -92,8 +91,12 @@ export default function Homepage(): JSX.Element {
                     </div>
                     <div className="repository__sync homepage__flex flex-l-5 flex-m-3">
                         <div className="repository__sync-img"/>
-                        <div className="repository__sync-title">Sync with Repository</div>
-                        <div className="repository__sync-description">Keep translations up to date – GitLocalize tracks changes in your repository and pulls them into the project. Translated files are synced back to the repo via a pull request.</div>
+                        <div className="repository__sync-title">2-Way Sync with the Repository</div>
+                        <div className="repository__sync-description">Your repository is the single source of truth. GitLocalize tracks changes in both the source and translated documents and pulls them into the project.
+                        Translations added on GitLocalize are sent to the repository via a pull request.
+                        <br/>  <br/>
+                        Have existing translations that need to be imported? No problem! Those will be synced into the project.
+                        </div>
                     </div>
 
                 </div>
@@ -103,9 +106,9 @@ export default function Homepage(): JSX.Element {
                 <div className="homepage__container--adaptive">
                     <div className="homepage__flex flex-l-10 flex-m-10">
                         <div className="parsing__img"/>
-                        <div className="parsing__title">Parsing and Segmentation</div>
-                        <div className="parsing__description">Easily see what needs to be translated – GitLocalize will parse your files into trackable text segments.</div>
-
+                        <div className="parsing__title">Continuous Translation of Updates</div>
+                        <div className="parsing__description">Easily see what parts of your document have been updated and need to be translated again in the side-by-side editor.
+                        <br/><br/>GitLocalize parses your files into translatable segments, making it easier to stay on top of updates and keep translations up-to-date.</div>
                         <div className="parsing__app-img-container">
                             <div className="parsing__app-img"/>
                         </div>
@@ -122,8 +125,23 @@ export default function Homepage(): JSX.Element {
                     </div>
                     <div className="teamwork__info homepage__flex flex-l-5 flex-m-3">
                         <div className="teamwork__info-img"/>
-                        <div className="teamwork__info-title">Teamwork</div>
-                        <div className="teamwork__info-description">Engage your team and community into the translation process <br/> – assign language moderators, send for review, and discuss changes. The same way you do it on GitHub.</div>
+                        <div className="teamwork__info-title">Community, Vendors and Teamwork</div>
+                        <div className="teamwork__info-description">Engage your community, work with in-house translators and vendors, or combine all three to build a sustainable translation pipeline.</div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="homepage__container cattools" id="cattools">
+                <div className="homepage__container--adaptive">
+                    <div className="homepage__flex flex-l-10 flex-m-10">
+                        <div className="cattools__img"/>
+                        <div className="cattools__title">Translation Consistency Tools</div>
+                        <div className="cattools__description">Ensure translation style and terminology are consistent by using Translation Memory and Glossaries.
+                        <br/><br/>In dynamically updating projects where a lot of text is reused across multiple documents, Translation Memory helps reduce translation costs and turnarund times.
+                        </div>
+                        <div className="cattools__app-img-container">
+                            <div className="cattools__app-img"/>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -132,7 +150,7 @@ export default function Homepage(): JSX.Element {
                 <div className="homepage__container--adaptive">
                     <div className="homepage__flex flex-l-10 flex-m-10 flex-s-10">
                         <div className="projects__title">Projects Already Using GitLocalize</div>
-                        <div className="projects__description">Easily see what needs to be translated – GitLocalize will parse your files into trackable text segments.</div>
+                        <div className="projects__description">Check out some of the awesome public projects that use Gitlocalize:</div>
                     </div>
                 </div>
 
@@ -152,15 +170,15 @@ export default function Homepage(): JSX.Element {
                     </div>
 
                     <div className="homepage__flex flex-l-3 flex-m-3 flex-s-10">
-                        <div className="project" id="rust">
+                        <div className="project" id="qiskit">
                             <div className="project__side">
-                                <div className="project__img vue_img"/>
-                                <a className="project__title project__link" href="https://gitlocalize.com/repo/2509">rust-lang-ru/rust-by-example</a>
-                                <div className="project__description">Rust by example</div>
+                                <div className="project__img qiskit_img"/>
+                                <a className="project__title project__link" href="https://gitlocalize.com/Qiskit/platypus">Qiskit/platypus</a>
+                                <div className="project__description">Open-Source Quantum Development</div>
                                 {/*<div className="project__languages">4 Languages</div>*/}
                             </div>
                             <div className="project__side project__arrow-container">
-                                <Button className="project__arrow" href="https://gitlocalize.com/repo/2509">{''}</Button>
+                                <Button className="project__arrow" href="https://gitlocalize.com/Qiskit/platypus">{''}</Button>
                             </div>
                         </div>
                     </div>
@@ -168,9 +186,9 @@ export default function Homepage(): JSX.Element {
                     <div className="homepage__flex flex-l-3 flex-m-3 flex-s-10">
                         <div className="project" id="tensorflow">
                             <div className="project__side">
-                                <div className="project__img nuxt_img"/>
+                                <div className="project__img tensorflow_img"/>
                                 <a className="project__title project__link" href="https://gitlocalize.com/repo/4592">tensorflow/docs-l10n</a>
-                                <div className="project__description">Tensorflow Documentation</div>
+                                <div className="project__description">An Open Source Machine Learning Framework for Everyone</div>
                                 {/*<div className="project__languages">12 Languages</div>*/}
                             </div>
                             <div className="project__side project__arrow-container">
@@ -191,7 +209,7 @@ export default function Homepage(): JSX.Element {
                         <div className="review__info-side flex-l-8 flex-m-6">
                             <div className="review__info-description">
                                 <div className="review__info-paragraph">
-                                    We use GitLocalize to localize our company WebAPI document. GitLocalize is extraordinarily convenient, flexible, and dynamic. We were pleasantly surprised to find that localization takes an extremely short time!
+                                    We use GitLocalize to localize our company's WebAPI documentation. GitLocalize is extraordinarily convenient, flexible, and dynamic. We were pleasantly surprised to find that localization takes an extremely short time!
                                 </div>
                                 <div className="review__info-paragraph">
                                     Translations can be managed statically on a file level and also can be compared to the original by segment. This makes managing the work absolutely seamless. We are able to create a consistent translation throughout the entire website with GitLocalize. To say we are impressed is an understatement.
@@ -208,8 +226,7 @@ export default function Homepage(): JSX.Element {
                 <div className="homepage__container--adaptive">
                     <div className="homepage__flex flex-l-10 flex-m-10">
                         <div className="advantages__title">How It Works</div>
-                        <div className="advantages__description">A better process to translate products, documentation or websites better process
-                            to translate products, documentation or websites</div>
+                        <div className="advantages__description">We made this short video to demonstrate how GitLocalize works</div>
                         <iframe className="advantages__video" id="demo" src="https://www.youtube.com/embed/PoSjHoEWV3w" frameBorder="0"
                                 allowFullScreen/>
                     </div>
@@ -219,7 +236,7 @@ export default function Homepage(): JSX.Element {
             <div className="homepage__container enjoy" id="enjoy">
                 <div className="homepage__container--adaptive">
                     <div className="homepage__flex flex-l-10 flex-m-10 flex-s-10">
-                        <div className="enjoy__title">Enjoy a Better Procees of Localization</div>
+                        <div className="enjoy__title">Create your first project now!</div>
                         {!authorization.token && <Button className="github-button enjoy__button" href="https://gitlocalize.com/auth/grant">Get started with GitHub</Button>}
                     </div>
                 </div>
@@ -237,8 +254,8 @@ export default function Homepage(): JSX.Element {
                                 <div className="footer__menu footer__features">
                                     <div className="footer__menu-title">Features</div>
                                     <div className="footer__menu-items">
-                                        <a className="footer__menu-item" href="#repository">Sync with Repository</a>
-                                        <a className="footer__menu-item" href="#parsing">Parsing & Segmentation</a>
+                                        <a className="footer__menu-item" href="#repository">2-Way Sync</a>
+                                        <a className="footer__menu-item" href="#parsing">Continuous Translation</a>
                                         <a className="footer__menu-item" href="#teamwork">Teamwork</a>
                                     </div>
                                 </div>
@@ -246,7 +263,7 @@ export default function Homepage(): JSX.Element {
                                     <div className="footer__menu-title">Contribute to Projects</div>
                                     <div className="footer__menu-items">
                                         <a className="footer__menu-item" href="#google">GoogleChrome/web.dev</a>
-                                        <a className="footer__menu-item" href="#rust">rust-lang-ru/rust-by-example</a>
+                                        <a className="footer__menu-item" href="#qiskit">Qiskit/platypus</a>
                                         <a className="footer__menu-item" href="#tensorflow">tensorflow/docs-l10n</a>
                                     </div>
                                 </div>
